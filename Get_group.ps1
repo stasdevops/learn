94e3fc6247e5f56caa468c1e35bd472e693e8c9c
tennessee <stas_devops@outlook.com>
2020-07-24 15:05:47 +0700

    Добавление комментария в строку

--- Get_group.ps1
+++ Get_group.ps1
@@ -1,3 +1,3 @@
 ﻿$user = Read-Host "Введите имя пользователя, чьи группы хотите получить" #Получаем список групп пользователей (DC\pushnik_as)
-Write-Host $user
+Write-Host $user "Выводим введёное имя"
 Get-ADUser -Identity $user -Properties memberof | Select-Object -ExpandProperty memberof 
